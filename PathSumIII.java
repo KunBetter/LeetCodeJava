b55c1a167d74f5011f449e9eb40f16578f9b1dbd
KunBetter <nk05xy@163.com>
2024-05-29 23:30:57 +0800

    PathSumIII

---
+++ PathSumIII.java
@@ -0,0 +1,43 @@
+/**
+ * 路径总和III
+ * 给定一个二叉树的根节点 root ，和一个整数 targetSum ，
+ * 求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+ * <p>
+ * 路径 不需要从根节点开始，也不需要在叶子节点结束，
+ * 但是路径方向必须是向下的（只能从父节点到子节点）。
+ */
+public class PathSumIII {
+
+    // 深度优先搜索
+    // 二叉树
+    public int pathSum(TreeNode root, int targetSum) {
+        /*
+         * 两种情况：① 经过根结点 ② 不经过根结点
+         */
+
+        if (root == null) {
+            return 0;
+        }
+
+        int ret = rootSum(root, targetSum);
+        ret += pathSum(root.left, targetSum);
+        ret += pathSum(root.right, targetSum);
+        return ret;
+    }
+
+    public int rootSum(TreeNode root, long targetSum) {
+        int ret = 0;
+
+        if (root == null) {
+            return 0;
+        }
+        int val = root.val;
+        if (val == targetSum) {
+            ret++;
+        }
+
+        ret += rootSum(root.left, targetSum - val);
+        ret += rootSum(root.right, targetSum - val);
+        return ret;
+    }
+}
